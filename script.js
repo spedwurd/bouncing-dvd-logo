a0c4bab8c3bb139e81fdecd20f926b1419b7c1c1
edzhang10 <edzhang10@gmail.com>
2024-07-22 10:32:43 -0400

    everythings fixed the dupe works NOT THE SPEED ONE

--- script.js
+++ script.js
@@ -1,54 +1,73 @@
-// i broke it trying to add multiple dvd functionality 😭
-
-let x = 1, y = 1, changeX = 3, changeY = 3;
+const x = [1], y = [1], changeX = [3], changeY = [3];
+var rate = 3;
 
 function changeColor(dvd) {
-  console.log(dvd);
-  let a, b, c;
+    let a, b, c;
     a = Math.floor(Math.random() * 256);
     b  = Math.floor(Math.random() * 256);
     c = Math.floor(Math.random() * 256);
     dvd.style.background = 'rgb(' + a + ',' + b + ',' + c + ')';
-    /* (clone dvd)
-    let p = document.getElementById('dvd');
+}
+
+function clone() {
+    console.log('bro what the sigma')
+    let p = document.getElementById('alpha');
     let p_prime = p.cloneNode(true);
-    console.log(p_prime.id); 
+    p_prime.id = "beta";
     p.after(p_prime);
-    */
+    x.push(Math.random() * (window.innerWidth-400));
+    y.push(Math.random() * (window.innerHeight-200));
+    changeX.push(3);
+    changeY.push(3);
 }
 
 function run() {
+    let current = 0;
     for (let dvd of document.getElementsByClassName('dvd')) {
-        move(dvd)
+        move(dvd, current);
+        current++;
     }
 }
 
-function move(dvd) {
-    dvd.style.top = y + 'px'; 
-    dvd.style.left = x + 'px';
-    x += changeX;
-    y += changeY;
-    if (y < -50) {
-        changeY = 3;
-        changeColor(dvd);
-    } else if (y > window.innerHeight-200) {
-        changeY = -3;
-        changeColor(dvd);
+function setSpeed() {
+    let speed = document.getElementById('speed').value;
+    rate = speed;
+    for (let s in changeX) {
+        changeX[s] *= (speed/changeX[s]);
     }
-    if (x < -50) {
-        changeX = 3;
-        changeColor(dvd);
-    } else if (x > window.innerWidth-400) {
-        changeX = -3;
-        changeColor(dvd);
+    for (let s in changeY) {
+        changeY[s] *= (speed/changeY[s]);
+
     }
 }
 
-console.log(document.getElementsByClassName('dvd'));
-window.onload = changeColor(document.getElementById('dvd'));
+function move(dvd, current) {
+    dvd.style.top = y[current] + 'px'; 
+    dvd.style.left = x[current] + 'px';
+    x[current] += changeX[current];
+    y[current] += changeY[current];
+    if (y[current] <= -50) {
+        console.log('bruuuh');
+        console.log(y[current]);
+        changeY[current] = rate;
+        changeColor(dvd);
+    } else if (y[current] > window.innerHeight-200) {
+        changeY[current] = rate * -1;
+        changeColor(dvd);
+    }
+    if (x[current] <= -50) {
+        changeX[current] = rate;
+        changeColor(dvd);
+    } else if (x[current] > window.innerWidth-400) {
+        changeX[current] = rate*-1;
+        changeColor(dvd);
+    }
+}
+document.getElementById('dvd')
+// window.onload = changeColor(document.getElementById('dvd'));
 setInterval(run, 10);
 /*
 todo:
-make a button that adds new dvds in (they need their own separate x-y/changex-y)
+make a button that adds new dvds in (they need their own separate x-y/changex-y)  DOOONE
 would prob be a loop going through the getElementbyClassNames, running move()/changecolor() individually
 */
